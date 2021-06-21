33c508d2ed97afbc23586a3559f309968bd995a4
Raj-Amrit-Singh <aj170896@gmail.com>
2021-06-21 17:59:27 +0530

    implemented basic cart

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -9,7 +9,7 @@ const ModalOverlay = props => {
     </div>)
 }
 const Backdrop = props => {
-  return <div className={classes.backdrop}></div>
+  return <div className={classes.backdrop} onClick={props.onClose}></div>
 };
 
 const portalElement = document.getElementById('overlays');
@@ -17,7 +17,7 @@ const portalElement = document.getElementById('overlays');
 const Modal = (props) => {
   return (
     <Fragment>
-      {ReactDom.createPortal(<Backdrop />, portalElement)}
+      {ReactDom.createPortal(<Backdrop onClose={props.onClose}/>, portalElement)}
       {ReactDom.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, portalElement)}
     </Fragment>
   )
