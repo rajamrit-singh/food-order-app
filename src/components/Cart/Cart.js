5c888a061fc1d24173f2908cc99f560f6af74c7d
Raj-Amrit-Singh <aj170896@gmail.com>
2021-06-22 11:24:52 +0530

    finished lecture 17

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,31 +1,39 @@
-import classes from './Cart.module.css';
-import Modal from '../UI/Modal';
 import { useContext } from 'react';
-import CartContext from '../../store/cart-context';
+
+import Modal from '../UI/Modal';
 import CartItem from './CartItem';
+import classes from './Cart.module.css';
+import CartContext from '../../store/cart-context';
 
 const Cart = (props) => {
   const cartCtx = useContext(CartContext);
 
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
-  
-  const cartItemRemoveHandler = (id) => {};
 
-  const cartItemAddHandler = (item) => {}; 
-  
-  const cartItems = <ul className={classes['cart-items']}>
-    {cartCtx.items.map(item =>
-      <CartItem
-        key={item.id}
-        name={item.name}
-        amount={item.amount}
-        price={item.price}
-        onRemove={cartItemRemoveHandler.bind(null, item.id)}
-        onAdd={cartItemAddHandler.bind(null, item)}>
-        {item.name}
-      </CartItem>)}
-  </ul>
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
+
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({...item, amount: 1});
+  };
+
+  const cartItems = (
+    <ul className={classes['cart-items']}>
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
+      ))}
+    </ul>
+  );
+
   return (
     <Modal onClose={props.onClose}>
       {cartItems}
@@ -34,7 +42,9 @@ const Cart = (props) => {
         <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes['button-alt']} onClick={props.onClose}>Close</button>
+        <button className={classes['button--alt']} onClick={props.onClose}>
+          Close
+        </button>
         {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
